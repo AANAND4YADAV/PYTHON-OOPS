bd1aa6731b5f6a3a55da0db5adca3e0a46858d64
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-05-16 06:26:34 +0000

    fixed the typo

--- @class method.py
+++ @class method.py
@@ -5,19 +5,19 @@ class Dog:
         self.name=name
     def bark(self):
         '''Returns a string of dog's name saying woof'''
-        return f"{self.name}says woof!"
+        return f"{self.name} says woof!"
 
 class Book:
     '''Create a book object, with a total book counter'''
     total_books=0
     def __init__(self,title):
-        '''Takesbook  name and increments total_book counter by 1'''
+        '''Takes book name and increments total_book counter by 1'''
         self.title=title
         Book.total_books+=1
     @classmethod
     def get_total_count(cls):
         '''Returns total book count'''
-        return f"Total books created:{cls.total_books}"
+        return f"Total books created : {cls.total_books}"
     @staticmethod
     def static_method(x,y):
         '''Adds 2 numbers'''
